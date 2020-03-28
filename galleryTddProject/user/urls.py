021a79b007a51a4d4e614424dc79460ff5868b45
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-28 15:18:00 -0500

    Paso 5: Login de una persona (Verde)

--- galleryTddProject/user/urls.py
+++ galleryTddProject/user/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:username>/gallery/', views.get_portafolio, name='get_portafolio')
+    path('<str:username>/gallery/', views.get_portafolio, name='get_portafolio'),
+    path('login/', views.login, name='login')
 ]
--- galleryTddProject/user/views.py
+++ galleryTddProject/user/views.py
@@ -1,6 +1,9 @@
+import json
+
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.views.decorators.csrf import csrf_exempt
 from gallery.models import Image
 
@@ -14,3 +17,18 @@ def get_portafolio(request, username):
     images_list = Image.objects.filter(public=public, user=user)
     return HttpResponse(serializers.serialize('json', images_list))
 
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        json_user = json.loads(request.body)
+        username = json_user['username']
+        password = json_user['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            user_list = User.objects.filter(username=username)
+            return HttpResponse(serializers.serialize("json", user_list))
+        else:
+            return HttpResponseNotFound('<h1>User not found</h1>')
