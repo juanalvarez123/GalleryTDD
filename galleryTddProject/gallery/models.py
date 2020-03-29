46dcf5e04ef8e336591594f5b14b222b6380d42c
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-29 12:55:28 -0500

    Paso 8: Agregar portafolio al usuario (Verde)

--- galleryTddProject/gallery/models.py
+++ galleryTddProject/gallery/models.py
@@ -1,8 +1,10 @@
+import json
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import QuerySet
 
 
-# Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=1000)
@@ -10,3 +12,8 @@ class Image(models.Model):
     type = models.CharField(max_length=5, blank=True)
     user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
     public = models.BooleanField(default=False, null=False)
+
+
+class Object:
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
--- galleryTddProject/gallery/views.py
+++ galleryTddProject/gallery/views.py
@@ -1,16 +1,40 @@
+import json
+
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.core import serializers
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.core import serializers
-from .models import Image
-import json
+
+from .models import Image, Object
+
 
 # Create your views here.
 @csrf_exempt
 def index(request):
-    images_list = Image.objects.all()
-    return HttpResponse(serializers.serialize("json", images_list))
+    if request.method == 'GET':
+        images_list = Image.objects.all()
+        return HttpResponse(serializers.serialize("json", images_list))
+    elif request.method == 'POST':
+        json_image = json.loads(request.body)
+        name = json_image['name']
+        url = json_image['url']
+        description = json_image['description']
+        type_image = json_image['type']
+        user_id = json_image['userId']
+
+        user = User.objects.filter(id=user_id)[0]
+
+        Image.objects.create(name=name, url=url, description=description, type=type_image, user=user)
+
+        image_response = Object()
+        image_response.quantity = len(Image.objects.filter(user=user))
+        image_response.image = Object()
+        image_response.image.name = name
+        image_response.image.url = url
+        image_response.image.description = description
+        image_response.image.type = type_image
+        return HttpResponse(image_response.to_json())
+
 
 @csrf_exempt
 def add_user_view(request):
